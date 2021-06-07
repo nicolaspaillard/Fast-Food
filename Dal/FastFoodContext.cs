cb159d9f7f0bc5868f3a3184ccd9e044bef67049
nicolas paillard <nicpaille@gmail.com>
2021-06-07 16:08:44 +0200

    update database

--- Dal/FastFoodContext.cs
+++ Dal/FastFoodContext.cs
@@ -29,5 +29,35 @@ namespace Dal
             //optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=FastFood;Integrated Security=true");
             base.OnConfiguring(optionsBuilder);
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Beverage>().ToTable("Beverages");
+            modelBuilder.Entity<Burger>().ToTable("Burgers");
+            modelBuilder.Entity<Dessert>().ToTable("Desserts");
+            modelBuilder.Entity<Menu>().ToTable("Menus");
+            modelBuilder.Entity<Side>().ToTable("Sides");
+
+            //modelBuilder.Entity<Person>()
+            //.HasKey(p => p.PersonId);
+
+            //modelBuilder.Entity<Person>()
+            //            .Property(p => p.PersonId)
+            //            .UseIdentityColumn();
+
+            //modelBuilder.Entity<Person>()
+            //            .Property(p => p.FirstName)
+            //            .HasMaxLength(30)
+            //            .IsRequired();
+
+            //modelBuilder.Entity<Classroom>()
+            //            .HasMany(c => c.Students)
+            //            .WithOne(s => s.Classroom);
+
+            modelBuilder.Entity<Menu>().HasOne(m => m.Beverage);
+            modelBuilder.Entity<Menu>().HasOne(m => m.Burger);
+            modelBuilder.Entity<Menu>().HasOne(m => m.Side);
+            modelBuilder.Entity<Menu>().HasOne(m => m.Dessert);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
